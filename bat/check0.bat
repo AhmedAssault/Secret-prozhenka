1b125ce40ac83630857a7557f33c03afd85851d8
AhmedAssault <76497893+AhmedAssault@users.noreply.github.com>
2025-12-27 13:28:54 +0300

    Update check0.bat

--- bat/check0.bat
+++ bat/check0.bat
@@ -1,10 +1,21 @@
 cls
-:: Check
-if "%~1"=="" (
-    call :test_service zapret
-    call :test_service WinDivert
+chcp 437 > nul
+
+sc query "zapret" >nul 2>&1
+if !errorlevel!==0 (
+    for /f "tokens=2*" %%A in ('reg query "HKLM\System\CurrentControlSet\Services\zapret" /v zapret-discord-youtube 2^>nul') do echo Service strategy installed from "%%B"
+)
+
+call :test_service zapret
+call :test_service WinDivert
+
+echo:
+
+tasklist /FI "IMAGENAME eq winws.exe" | find /I "winws.exe" > nul
+if !errorlevel!==0 (
+    call :PrintGreen "Bypass (winws.exe) is RUNNING."
 ) else (
-    call :test_service "%~1" "soft"
+    call :PrintRed "Bypass (winws.exe) is NOT running."
 )
 
 exit /b
@@ -14,19 +25,22 @@ set "ServiceName=%~1"
 set "ServiceStatus="
 
 for /f "tokens=3 delims=: " %%A in ('sc query "%ServiceName%" ^| findstr /i "STATE"') do set "ServiceStatus=%%A"
-
 set "ServiceStatus=%ServiceStatus: =%"
 
 if "%ServiceStatus%"=="RUNNING" (
     if "%~2"=="soft" (
-        echo "%ServiceName%" is ALREADY RUNNING as service!
+        echo "%ServiceName%" is ALREADY RUNNING as service, use "service.bat" and choose "Remove Services" first if you want to run standalone bat.
         pause
+        exit /b
     ) else (
-   call :PrintGreen ""%ServiceName%" Служба запущена."
+        echo "%ServiceName%" service is RUNNING.
     )
+) else if "%ServiceStatus%"=="STOP_PENDING" (
+    call :PrintYellow "!ServiceName! is STOP_PENDING, that may be caused by a conflict with another bypass. Run Diagnostics to try to fix conflicts"
 ) else if not "%~2"=="soft" (
-   call :PrintRed ""%ServiceName%" Служба не запущена."
+    echo "%ServiceName%" service is NOT running.
 )
+
 exit /b
 
 :: Colors
